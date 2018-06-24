980ede334d79f59f061fdd88eb5e9190ad735e63
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-24 17:26:30 +0900

    css edit

--- app/members/views.py
+++ app/members/views.py
@@ -33,13 +33,20 @@ def sign_up(request):
         password2 = request.POST['password2']
         email = request.POST['email']
 
-
+        # 비밀번호 확인하기
         if password == password2:
+            # 일치할 경우에 계정 생성
             User.objects.create_user(username=username, password=password2, email=email, last_name=last_name,)
+            # 생성후 바로 로그인 처리하기
             user = authenticate(request, username=username, password=password2,)
+            # 로그인
             login(request, user)
+            # defult page 로 이동
             return redirect('posts:post-list')
         else:
+            # 비밀번호 확인이 일치하지 않을 경우에 메세지 리턴,
             return HttpResponse('비밀번호가 일치하지 않습니다.')
     else:
+        # signup 템플릿 불러오기
+        # 일단 POST 로 request 가 오지 않았기 때문에 우선 실행하여 else 문에 넣었다.
         return render(request, 'members/signup.html')
