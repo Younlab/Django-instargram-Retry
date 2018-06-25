81cb4b379c5e6484528882ebd57eeca9697b6fc8
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-25 12:28:41 +0900

    signup views

--- app/members/views.py
+++ app/members/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+# User 클래스 자체를 가져올때는 get_user_model()
+# ForeignKey에 User 모델을 지정할때는 settings.AUTH_USER_MODEL
+User = get_user_model()
 
 def login_view(request):
     if request.method == 'POST':
@@ -24,8 +26,10 @@ def logout_view(request):
     else:
         return redirect('posts:post-list')
 
-
 def sign_up(request):
+    context = {
+        'errors': [],
+    }
     if request.method == 'POST':
         username = request.POST['username']
         last_name = request.POST['last_name']
@@ -33,20 +37,30 @@ def sign_up(request):
         password2 = request.POST['password2']
         email = request.POST['email']
 
+        context['username']=username
+        context['email']=email
+        # exists를 사용해서 유저가 이미 존재하면 signup으로 다시 redirect
+        # 존재하지 않는 경우에만 아래 로직 실행
+        # 일치할 경우에 계정 생성
+
+        if User.objects.all().filter(username=username).exists():
+            # 단순 redirect 가 아니라, render를 사용
+            # render 에 context를 전달
+            # errors list에 '유저가 이미 존재함'
+            # 템플릿에서는 전달받은 errors를 순회하며 에러메세지를 출력
+
+            context['username'].append(username)
+            context['email'].append(email)
+            context['errors'].append('유저가 이미 존재합니다.')
+
         # 비밀번호 확인하기
-        if password == password2:
-            # 일치할 경우에 계정 생성
-            User.objects.create_user(username=username, password=password2, email=email, last_name=last_name,)
-            # 생성후 바로 로그인 처리하기
-            user = authenticate(request, username=username, password=password2,)
-            # 로그인
+        if password != password2:
+            context['errors'].append('비밀번호가 일치하지 않습니다.')
+
+        if not context['errors']:
+
+            user = User.objects.create_user(username=username, password=password2, email=email, last_name=last_name,)
             login(request, user)
-            # defult page 로 이동
             return redirect('posts:post-list')
-        else:
-            # 비밀번호 확인이 일치하지 않을 경우에 메세지 리턴,
-            return HttpResponse('비밀번호가 일치하지 않습니다.')
-    else:
-        # signup 템플릿 불러오기
-        # 일단 POST 로 request 가 오지 않았기 때문에 우선 실행하여 else 문에 넣었다.
-        return render(request, 'members/signup.html')
+
+    return render(request, 'members/signup.html', context)
