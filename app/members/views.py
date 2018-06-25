b517985c2fd52331714e924e97837d682666df45
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-25 12:30:12 +0900

    signup end

--- app/members/views.py
+++ app/members/views.py
@@ -49,8 +49,6 @@ def sign_up(request):
             # errors list에 '유저가 이미 존재함'
             # 템플릿에서는 전달받은 errors를 순회하며 에러메세지를 출력
 
-            context['username'].append(username)
-            context['email'].append(email)
             context['errors'].append('유저가 이미 존재합니다.')
 
         # 비밀번호 확인하기
